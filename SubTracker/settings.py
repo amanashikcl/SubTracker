33fb644b3d7d111ecee19b4dda17760924633b95
amanashikcl <amanashikcloud@outlook.com>
2026-01-02 20:29:03 +0530

    added whitenoise

--- SubTracker/settings.py
+++ SubTracker/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,7 +156,7 @@ LOGIN_REDIRECT_URL = 'dashboard' # Redirect to dashboard after login
 
 # ... existing code ...
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ADD THIS LINE:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
