1b56d2575545c3c8f54539b025f2c7ed8f401b7f
amanashikcl <amanashikcloud@outlook.com>
2026-01-02 20:20:09 +0530

    added debug

--- SubTracker/settings.py
+++ SubTracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6l^x^$_+&m14i=70(0+gnj1#wv+0(#^1o_wipen60g)d0bjk%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.108.252.12']
 
